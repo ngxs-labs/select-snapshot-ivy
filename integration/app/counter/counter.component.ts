d506fef72bf23da8c19644eb6efc78d131e2b0bd
arturovt <arthurandrosovich@gmail.com>
2019-08-30 01:34:45 +0300

    fix: integration useless property

--- integration/app/counter/counter.component.ts
+++ integration/app/counter/counter.component.ts
@@ -1,7 +1,4 @@
 import { Component, ChangeDetectionStrategy, Input, OnDestroy } from '@angular/core';
-import { ViewSelectSnapshot } from '@ngxs-labs/select-snapshot-ivy';
-
-import { CounterState } from '../counter.state';
 
 @Component({
   selector: 'app-counter',
@@ -9,8 +6,6 @@ import { CounterState } from '../counter.state';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CounterComponent implements OnDestroy {
-  @ViewSelectSnapshot(CounterState.getCounter) counterz!: number;
-
   @Input() ivyEnabled: boolean = null!;
 
   @Input() counter: number = null!;
